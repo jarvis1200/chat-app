7de47f070c48e609c6e648345dca75fe25017e8e
jarvis1200 <61680475+jarvis1200@users.noreply.github.com>
2020-05-21 05:17:02 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:chatApp/screen/home_Screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.red,
+        accentColor: Color(0xFFFEF9EB),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
